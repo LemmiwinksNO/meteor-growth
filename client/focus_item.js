b0f6cd39c060ee78ae2b8d41db6128f859a370eb
LemmiwinksNO <macreasy@gmail.com>
2014-04-20 08:39:19 -0500

    [#5] User can add tasks
    Added event handler to focusItem that listens for a click on '.new-task' button. We have all the data in the data-context except title, which we grab from the dom.
    Then call 'newTask' method which handles the update to db.tasks.

--- client/focus_item.js
+++ client/focus_item.js
@@ -10,5 +10,26 @@ Template.focusItem.events({
     // Set this focus on session to update focus-modal
     Session.set('currentFocus', this);
     $('#focus-modal').modal();
+  },
+
+  'click .new-task': function(e) {
+    var now = new Date();
+    var $title = $(e.target).parents('.input-group').find('[name=new-task-title]');
+
+    var task = {
+      title: $title.val(),
+      focusId: this._id,
+      userId: this.userId,
+      completed: false,
+      created: now
+    };
+
+    Meteor.call('newTask', task, function(error, id) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        $title.val('');
+      }
+    });
   }
 });
--- collections/tasks.js
+++ collections/tasks.js
@@ -3,3 +3,21 @@ if (typeof db === 'undefined')
   db = {};
 
 db.tasks = new Meteor.Collection('tasks');
+
+Meteor.methods({
+  'newTask': function(task) {
+    var user = Meteor.user();
+
+    // Ensure user is logged in
+    if (!user)
+      throw new Meteor.error(401, "You need to log in to create tasks");
+
+    // Ensure task has a title
+    if (!task.title)
+      throw new Meteor.error(422, "Please fill in a title");
+
+    var taskId = db.tasks.insert(task);
+
+    return taskId;
+  }
+});
